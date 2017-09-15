9a3af23d5ed288086100bb53179011b8ffdfdd1b
Your Name <you@example.com>
2017-09-15 10:34:58 +0530

    Project Added

---
+++ Project/B.java
@@ -0,0 +1,9 @@
+public classhyd {
+
+public static void main(String[] args)
+{
+System.out.println("Welcome to master");
+System.out.println("Welcome to tester");
+}
+
+}
---
+++ Project/Tester.java
@@ -0,0 +1,8 @@
+class Tester{
+
+
+public void getData()
+{
+System.out.println("In Tester");
+}
+}
